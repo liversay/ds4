7668d5439c9d04027088542b5f2671942220ad93
liversay <sespinoc131005@gmail.com>
2024-08-30 17:19:41 -0500

    commit inicial - lab2

---
+++ Laboratorio2/Laboratorio2/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Definición de método que suma dos números e imprime el resultado.
+        public void Sumar(int valor1, int valor2) {
+            int variableLocal = valor1 + valor2; //Declarando la variable local
+            Console.WriteLine(variableLocal); //Esto imprime el resultado de la suma.
+        }
+        Console.WriteLine(variableLocal); //Esto presenta un error de compilación
+    }
+}
