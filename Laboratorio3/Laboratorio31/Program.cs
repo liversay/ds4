ffc7f3d10c3b5f3f2b5a85f42d3ae4d28abad6f3
liversay <sespinoc131005@gmail.com>
2024-09-04 01:07:39 -0500

    Laboratorio 31

--- Laboratorio3/Laboratorio31/Program.cs
+++ Laboratorio3/Laboratorio31/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double val1, val2;
+        Console.WriteLine("Ingrese el primer valor: ");
+        val1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el segundo valor: ");
+        val2 = Convert.ToInt32(Console.ReadLine());
+        CalculosMatematicos obj = new CalculosMatematicos();
+        obj.Calcular(val1, val2);
+
+    }
+
+    public class CalculosMatematicos
+    {
+        public double Calcular(double a, double b)
+        {
+            double resultado;
+            resultado = (a + b) * (a - b);
+            return resultado;  
+        }
+    }
+}
